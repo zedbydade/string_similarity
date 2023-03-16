155ed2d19bc09e68f93150471af090ba8fdc017e
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-03-15 22:50:51 -0300

    First Commit

---
+++ lib/string_similarity.ex
@@ -0,0 +1,18 @@
+defmodule StringSimilarity do
+  @moduledoc """
+  Documentation for `StringSimilarity`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StringSimilarity.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/string_similarity_test.exs
@@ -0,0 +1,8 @@
+defmodule StringSimilarityTest do
+  use ExUnit.Case
+  doctest StringSimilarity
+
+  test "greets the world" do
+    assert StringSimilarity.hello() == :world
+  end
+end
