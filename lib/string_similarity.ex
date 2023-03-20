5b5dfb63476bd7c05397673beb8e030a5819a752
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-03-19 23:16:00 -0300

    feat: Add get_vector and start get_dot implementation

--- lib/string_similarity.ex
+++ lib/string_similarity.ex
@@ -1,18 +1,23 @@
+require Iter
+
 defmodule StringSimilarity do
-  @moduledoc """
-  Documentation for `StringSimilarity`.
-  """
+  # Calcualte the 
+  #  https://en.wikipedia.org/wiki/Cosine_similarity Cosine
+  #  of two strings.
 
-  @doc """
-  Hello world.
+  def cosine(string_1, string_2) when string_1 == string_2 do 
+    1.0
+  end
 
-  ## Examples
+  def cosine(string_1, string_2) when string_1 == "" or string_2 == "" do 
+    0.0
+  end
 
-      iex> StringSimilarity.hello()
-      :world
+  def cosine(string_1, string_2) do
+    vector_1 = Iter.traverse(String.split(string_1), &Iter.to_vector/2)
+    vector_2 = Iter.traverse(String.split(string_2), &Iter.to_vector/2)
 
-  """
-  def hello do
-    :world
+    dot_result = get_dot(vector_1, vector_2)
+
   end
 end
